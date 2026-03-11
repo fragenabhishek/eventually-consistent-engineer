12ea98fe7c8553d90791b445326812eb2f4e9bcf
fragenabhishek <fragenabhishek@gmail.com>
2026-03-11 18:14:05 +0530

    Add DesignPatterns folder

--- src/main/java/com/fragenabhishek/designpatterns/creational/DatabaseConnection.java
+++ src/main/java/com/fragenabhishek/designpatterns/creational/DatabaseConnection.java
@@ -7,24 +7,39 @@ public class DatabaseConnection {
     private String password;
 
 
-    private DatabaseConnection() {
-        // private constructor
+    private DatabaseConnection(String url, String username, String password) {
+        this.url = url;
+        this.username = username;
+        this.password = password;
     }
 
-    public static synchronized DatabaseConnection getInstance() {
+    public static synchronized DatabaseConnection getInstance(String url, String username, String password) {
         if (instance == null) {
-            instance = new DatabaseConnection();
+            instance = new DatabaseConnection(url,username,password);
+        }
+        return instance;
+    }
+
+    public static DatabaseConnection getInstance() {
+        if (instance == null) {
+            throw new IllegalStateException("DatabaseConnection is not initialized. Call getInstance(url, username, password) first.");
         }
         return instance;
     }
 
     public void showMessage() {
-        System.out.println("Singleton database connectin!");
+        System.out.println("Singleton database connection!");
+        System.out.println("URL: " + url);
+        System.out.println("Username: " + username);
     }
 
     public static void main(String[] args) {
-        DatabaseConnection singleton = DatabaseConnection.getInstance();
+        DatabaseConnection singleton = DatabaseConnection.getInstance(
+                "jdbc:mysql://localhost:3306/mydb", "admin", "password123"
+        );
         singleton.showMessage();
+
+        // Subsequent calls: no need to provide credentials
         DatabaseConnection db1 = DatabaseConnection.getInstance();
         DatabaseConnection db2 = DatabaseConnection.getInstance();
 
